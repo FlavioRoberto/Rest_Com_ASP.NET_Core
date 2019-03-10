622b176d4b0ea6950e2e6de2b3cfde54bf272152
Flávio Roberto Teixeira <flaviolive3@hotmail.com>
2019-03-10 19:49:32 -0300

    Finalização calculadora

--- Api/Api/Controllers/CalculadoraController.cs
+++ Api/Api/Controllers/CalculadoraController.cs
@@ -1,5 +1,6 @@
 ﻿using Api.Extensions;
 using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Threading.Tasks;
 
 namespace Api.Controllers
@@ -12,12 +13,48 @@ namespace Api.Controllers
         [HttpGet()]
         public async Task<IActionResult> Soma([FromQuery] string primeiroNumero, [FromQuery] string segundoNumero)
         {
-            if (!primeiroNumero.EhNumerico())
-                return BadRequest("Primeiro número inválido");
+            string numerosValidos = ValidarNumeros(primeiroNumero, segundoNumero);
+            if (numerosValidos.Length > 0)
+                return BadRequest(numerosValidos);
+            return CalcularOperacao("adicao", primeiroNumero, segundoNumero);
+        }
 
-            if (!segundoNumero.EhNumerico())
-                return BadRequest("Segundo número inválido");
 
+        [Route("subtracao")]
+        [HttpGet()]
+        public async Task<IActionResult> Subtracao([FromQuery] string primeiroNumero, [FromQuery] string segundoNumero)
+        {
+            string numerosValidos = ValidarNumeros(primeiroNumero, segundoNumero);
+            if (numerosValidos.Length > 0)
+                return BadRequest(numerosValidos);
+
+            return CalcularOperacao("subtracao", primeiroNumero, segundoNumero);
+        }
+
+        [Route("multiplicacao")]
+        [HttpGet()]
+        public async Task<IActionResult> Multiplicacao([FromQuery] string primeiroNumero, [FromQuery] string segundoNumero)
+        {
+            string numerosValidos = ValidarNumeros(primeiroNumero, segundoNumero);
+            if (numerosValidos.Length > 0)
+                return BadRequest(numerosValidos);
+
+            return CalcularOperacao("multiplicacao", primeiroNumero, segundoNumero);
+        }
+
+        [Route("divisao")]
+        [HttpGet()]
+        public async Task<IActionResult> Divisao([FromQuery] string primeiroNumero, [FromQuery] string segundoNumero)
+        {
+            string numerosValidos = ValidarNumeros(primeiroNumero, segundoNumero);
+            if (numerosValidos.Length > 0)
+                return BadRequest(numerosValidos);
+
+            return CalcularOperacao("divisao", primeiroNumero, segundoNumero);
+        }
+
+        private IActionResult CalcularOperacao(string operacao, string primeiroNumero, string segundoNumero)
+        {
             Retorno<decimal> conversaoPrimeiroNumero = primeiroNumero.ToDecimal();
             Retorno<decimal> conversaoSegundoNumero = segundoNumero.ToDecimal();
 
@@ -27,8 +64,40 @@ namespace Api.Controllers
             if (conversaoSegundoNumero.ExisteErro())
                 return BadRequest(conversaoSegundoNumero.GetErro());
 
-            return Ok("A soma dos números informados é: " +
-                $"{conversaoPrimeiroNumero.Resultado + conversaoSegundoNumero.Resultado}");
+
+            switch (operacao)
+            {
+                case "adicao":
+                    return Ok("A soma dos números informados é: " +
+                             $"{conversaoPrimeiroNumero.Resultado + conversaoSegundoNumero.Resultado}");
+                case "subtracao":
+                    return Ok("A subtração dos números informados é: " +
+                             $"{conversaoPrimeiroNumero.Resultado - conversaoSegundoNumero.Resultado}");
+                case "multiplicacao":
+                    return Ok("A multiplicação dos números informados é: " +
+                         $"{conversaoPrimeiroNumero.Resultado * conversaoSegundoNumero.Resultado}");
+                case "divisao":
+                    if (conversaoSegundoNumero.Resultado == 0)
+                        return BadRequest("Não é possível dividir por 0!");
+
+                    return Ok("A multiplicação dos números informados é: " +
+                        $"{conversaoPrimeiroNumero.Resultado / conversaoSegundoNumero.Resultado}");
+
+                default: return BadRequest("Operação não implementada!");
+            }
         }
+
+        private string ValidarNumeros(string primeiroNumero, string segundoNumero)
+        {
+            if (!primeiroNumero.EhNumerico())
+                return "Primeiro número inválido";
+
+            if (!segundoNumero.EhNumerico())
+                return "Segundo número inválido";
+
+            return string.Empty;
+        }
+
     }
+
 }
